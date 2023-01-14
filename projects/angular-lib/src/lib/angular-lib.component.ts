ba6293d83381109d9c160f73d5be115bc1307595
Bernhard Pottler <bpottler@nexgo.de>
2023-01-14 16:43:30 +0100

    build: add projects to workspace

---
+++ projects/angular-lib/src/lib/angular-lib.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular-lib',
+  template: `
+    <p>
+      angular-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AngularLibComponent {
+
+}
